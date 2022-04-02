06e4035d60af275b5655b180c70a7113afd3ade8
Vu Ho <vh.ho8160@gmail.com>
2022-04-02 13:25:55 -0400

    Update constant.py

--- constant.py
+++ constant.py
@@ -1,5 +1,11 @@
 # Define constant variables
-
-TRUNC_TYPE='post'
-PADDING='post'
+# Change to actual value when model up
+LABEL_NAME = 'sentiment'
+INPUT_NAME = 'review'
+TEST_SIZE = 0.2
+VOCAB_SIZE = 10000
+MAX_LENGTH = 1000
+TRUNC_TYPE = 'post'
+PADDING = 'post'
 OOV = '<OOV>'
+data_path = './data/raw/IMDB Dataset.csv'
